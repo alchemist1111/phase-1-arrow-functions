9d3210bf6b8ec4faaab32802bd2d1f01df08a6b5
eugene-emoyo-ojakaa <emoyoeugine@gmail.com>
2024-03-21 12:54:22 +0300

    adds index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+// Fuction expression for division.
+let a = 2000
+let b = 100
+const divide = function(a, b) {
+   
+    return a/b;
+} 
+// Arrow function for multiplication.
+let x = 2;
+const square = x => {
+    return x * x;
+}
+//Arrow function for addition.
+let z = 3;
+let y = 4;
+ 
+const add = (z, y) => {
+    return z + y;
+}
+    
